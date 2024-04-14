921149d4b9997a089d04cf7de4ae49591e482931
Anton <forsakenanton@gmail.com>
2024-04-14 16:18:07 +0300

    6 Building Forms with Validation

---
+++ Components/RazorComponents/BootstrapFieldCssClassProvider.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace Components;
+public class BootstrapFieldCssClassProvider : FieldCssClassProvider
+{
+    public override string GetFieldCssClass(
+        EditContext editContext,
+        in FieldIdentifier fieldIdentifier)
+    {
+        bool isValid = !editContext
+            .GetValidationMessages(fieldIdentifier)
+            .Any();
+
+        bool isModified = editContext.IsModified(fieldIdentifier);
+
+        return (isModified, isValid) switch
+        {
+            (true, true) => "form-control modified is-valid",
+            (true, false) => "form-control modified is-invalid",
+            (false, true) => "form-control",
+            (false, false) => "form-control"
+        };
+    }
+}
