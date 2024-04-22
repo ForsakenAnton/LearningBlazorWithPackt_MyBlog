1022c349785bee6f683b9476721396ef23b521b7
Anton <forsakenanton@gmail.com>
2024-04-22 15:05:13 +0300

    Creating the client

--- Data.Models/Interfaces/IBlogApi.cs
+++ Data.Models/Interfaces/IBlogApi.cs
@@ -5,7 +5,7 @@ public interface IBlogApi
     Task<int> GetBlogPostCountAsync();
 
     //Task<List<BlogPost>?> GetBlogPostsAsync(int numberofposts, int startindex);
-    Task<List<BlogPost>> GetBlogPostsAsync(int numberofposts, int startindex);
+    Task<List<BlogPost>?> GetBlogPostsAsync(int numberofposts, int startindex);
     Task<List<Category>?> GetCategoriesAsync();
     Task<List<Tag>?> GetTagsAsync();
 
