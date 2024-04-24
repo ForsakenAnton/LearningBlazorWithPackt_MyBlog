349b3fa24a9a5ee184fe049ccee5091a17c4a310
Anton <forsakenanton@gmail.com>
2024-04-24 13:26:56 +0300

    Securing Blazor WebAssembly

--- BlazorWebAssembly/Client/Program.cs
+++ BlazorWebAssembly/Client/Program.cs
@@ -1,11 +1,39 @@
-using BlazorWebAssembly.Client;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+using BlazorWebAssembly.Client;
+using Data;
+using Data.Models.Interfaces;
+using Components.RazorComponents;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddTransient<IBlogApi, BlogApiWebClient>();
+
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddHttpClient("Public", client =>
+{
+    client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+});
+
+builder.Services.AddHttpClient("Authenticated", client =>
+{
+    client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+})
+.AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+builder.Services.AddOidcAuthentication(options =>
+{
+    builder.Configuration.Bind("Auth0", options.ProviderOptions);
+    options.ProviderOptions.ResponseType = "code";
+});
+
+builder.Services.AddTransient<ILoginStatus, LoginStatusWasm>();
 
 await builder.Build().RunAsync();
