236c2d93b84b37498ea7ce781f3c907de67b8daf
Anton <forsakenanton@gmail.com>
2024-04-28 16:23:06 +0300

    JavaScript interop in WebAssembly. .NET to JavaScript

---
+++ BlazorWebAssembly/Client/JSInteropSamples/NetToJs.razor.cs
@@ -0,0 +1,9 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+
+namespace BlazorWebAssembly.Client.JSInteropSamples;
+
+public partial class NetToJs
+{
+    [JSImport("showAlert", "nettojs")]
+    internal static partial string ShowAlert(string message);
+}
